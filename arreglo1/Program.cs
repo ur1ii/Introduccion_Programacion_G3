ae5abbdc03c3b55baa08c56888384de3dac0854a
HUMBERTO <hrruiz@uamv.edu.ni>
2026-05-06 10:05:57 -0600

    arreglo1

---
+++ arreglo1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Diagnostics;
+
+/// Almacenar 10 edades sen un arreglo, luego calcular el promedio de edades, el maximo
+/// y el minimo.
+int[] edades = new int[10]; 
+
+//Pedir edades 
+for (int i = 0; i < edades.Length; i++)
+{
+    try
+    {
+        Console.WriteLine($"Ingrese la edad {i + 1}: ");
+        edades[i] = int.Parse(Console.ReadLine()!);
+    }
+    catch (FormatException)
+    {   
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero.");
+        i--;
+        Console.ResetColor();
+    }
+int suma = 0, maximo = edades[0], minimo = edades[0];
+    foreach (int edad in edades)
+    {
+        suma += edad;
+        if (edad > maximo)
+        {
+            maximo = edad;
+        }
+        if (edad < minimo || minimo == 0)
+        {
+            minimo = edad;
+        }
+        double promedio = (double)suma / edades.Length;
+Console.WriteLine("El promedio de edades es: " + promedio);
+Console.WriteLine("La edad máxima es: " + maximo);
+Console.WriteLine("La edad mínima es: " + minimo);
+    }
+}
---
+++ arreglo2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Crea un arreglo de notas de 15 estudiantes, luego calcula el promedio de notas
+// y los 3 primeros luagres. no se puede ingresar notas menores a 0 ni mayores a 100
+int[] notas = new int[15];
+int suma = 0;
+double promedio = 0;
+for (int i = 0; i < notas.Length; i++)
+{
+    try
+    {
+        Console.WriteLine("Ingrese la notra del estudiante:" + (i + 1));
+        int nota = int.Parse(Console.ReadLine());
+        if(nota < 0 || nota > 100)
+        {
+            throw new Exception("La nota debe ser entre 0 y 100");
+        }   
+        notas[i] = nota;
+        suma += nota;
+
+    }
+    catch(ArgumentOutOfRangeException)
+    {
+        Console.WriteLine("Error: La nota debe ser entre 0 y 100");
+        i--; 
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Error: Debe ingresar un número válido");
+        i--; 
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+        i--;
+    }
+}
