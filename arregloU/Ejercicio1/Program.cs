04eb0ddc2fe3ec0ce6e9f8c7fb3526ad7612ccaa
HUMBERTO <hrruiz@uamv.edu.ni>
2026-05-11 10:43:23 -0600

    ArreglosU

---
+++ arregloU/Ejercicio1/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] temp = new int[7];
+int mayor = int.MinValue, menor = int.MaxValue;
+int temps = 0;
+
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Ingresa las temperaturas");
+Console.ResetColor();
+for (int i = 0; i < temp.Length; i++)
+{
+    try
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"temperatura #{i + 1}");
+        Console.ResetColor();
+        temp[i] = int.Parse(Console.ReadLine());
+        temps += temp[i];
+    }
+    catch (FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("ERROR, Dato invalido");
+        Console.ResetColor();
+        i--;
+        continue;
+    }
+    
+    if (temp[i] > mayor)
+    {
+        mayor = temp[i];
+    }
+    if (temp[i] < menor)
+    {
+        menor = temp[i];
+    }
+
+}
+double promedio = (double)temps / temp.Length;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"El promedio de las temperaturas es: {promedio:F2}");
+Console.WriteLine("La temperatura mas alta es: " + mayor);
+Console.WriteLine("La temperatura mas baja es: " + menor);
+Console.ResetColor();
+
---
+++ arregloU/Ejercicio2/Program.cs
@@ -0,0 +1,45 @@
+﻿//Ventas de una tienda
+
+int[] ventas = new int[7];
+int total=0;
+int menor =int.MaxValue;
+int diaS=0;
+
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Ingresa las ventas del dia");
+Console.ResetColor();
+for(int i=0; i < ventas.Length; i++)
+{
+   try
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"Venta #{i+1}");
+        ventas[i] = int.Parse(Console.ReadLine());
+        Console.ResetColor();
+        total += ventas[i];
+    }
+    catch(FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("ERROR. Reingrese los datos");
+        Console.ResetColor();
+        i--;
+    }
+
+    if(ventas[i] > 150)
+    {
+        diaS++;
+    }
+    if(ventas[i] < menor)
+    {
+        menor = ventas[i];
+    }
+    
+}
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("EL total vendido fue: "+total);
+    Console.WriteLine("Dias en los que las ventas superaron 150: "+diaS);
+    Console.WriteLine("La venta mas baja registrada fue: "+menor);
+    Console.ResetColor();
