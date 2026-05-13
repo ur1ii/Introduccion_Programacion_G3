e571d6bea5814d60b8ac931a03ae14444cbda3b8
HUMBERTO <hrruiz@uamv.edu.ni>
2026-05-13 10:42:19 -0600

    Matriz

---
+++ Matriz1/Program.cs
@@ -0,0 +1,47 @@
+﻿/// Matriz 3x3
+
+Console.Clear();
+
+int[,] matriz = new int[3, 3];
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Ingrese los elementos de la matriz 3x3");
+Console.ResetColor();
+for (int f = 0; f < 3; f++)
+{
+    for (int c = 0; c < 3; c++)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"Elemento [{f},{c}]");
+        Console.ResetColor();
+        matriz[f, c] = int.Parse(Console.ReadLine()!);
+        
+    }
+
+}
+Console.WriteLine("Mostrar matriz");
+for (int f = 0; f < 3; f++)
+{
+    for(int c = 0; c <3; c++)
+    {
+        if(matriz[f,c] % 2 == 0 && matriz[f, c] >0)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{matriz[f, c]} ");
+            Console.ResetColor();
+        }
+        else if(matriz[f,c] % 2 != 0 && matriz[f, c] >0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{matriz[f, c]} ");
+            Console.ResetColor();
+        }
+        else if(matriz[f,c] <0)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write($"{matriz[f, c]} ");
+            Console.ResetColor();
+        }
+        
+    }
+    Console.WriteLine();
+}
---
+++ Matriz2/Program.cs
@@ -0,0 +1,34 @@
+﻿int[,] matriz = new int[5, 5];
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Ingrese los datos de la matriz 5x5");
+Console.ResetColor();
+for(int f=0; f<5; f++)
+{
+    for(int c=0; c<5; f++)
+    {
+        try
+        {   
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"Elemento {matriz[f, c]}");
+            Console.ResetColor();
+            matriz[f, c] = int.Parse(Console.ReadLine());
+        }
+        catch(FormatException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR, reingrese los datos");
+            Console.ResetColor();
+        }
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine(" === Mostrar matriz ===");
+        Console.ResetColor();
+        for(int f=0; f<5; f++)
+        {
+
+            for( int c=0; c<5; c++)
+            {
+                Console.WriteLine("Su numero esta en la matriz");
+            }
+        }
+    }
+}
